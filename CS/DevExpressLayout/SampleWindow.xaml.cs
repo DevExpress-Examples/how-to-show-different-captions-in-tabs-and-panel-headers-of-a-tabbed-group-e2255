a86e42df7dd56edcbcc549ae69446dea7dbf44e4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DevExpressLayout/SampleWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Docking;
+
+namespace DevExpressLayout
+{
+    /// <summary>
+    /// Interaction logic for SampleWindow.xaml
+    /// </summary>
+    public partial class SampleWindow : Window {
+
+        //public static readonly DependencyProperty TabCaptionProperty;
+
+        //static SampleWindow() {
+        //    TabCaptionProperty = DependencyProperty.RegisterAttached("TabCaption", typeof(string), typeof(SampleWindow));
+        //}
+
+        //public static string GetTabCaption(DependencyObject obj) {
+        //    return (string)obj.GetValue(TabCaptionProperty);
+        //}
+        //public static void SetTabCaption(DependencyObject obj, string value) {
+        //    obj.SetValue(TabCaptionProperty, value);
+        //}
+
+        public SampleWindow() {
+            InitializeComponent();
+            
+            //pnlProduct.CaptionTemplate = FindResource("AzureCaptionTemplate") as DataTemplate;
+            //pnlEmployee.CaptionTemplate = FindResource("AzureCaptionTemplate") as DataTemplate;
+
+            //SetTabCaption(pnlProduct, "Tab 1");
+            //SetTabCaption(pnlEmployee, "Tab 2");
+        }
+    }
+}
---
+++ VB/DevExpressLayout/SampleWindow.xaml.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Shapes
+Imports DevExpress.Xpf.Docking
+
+Namespace DevExpressLayout
+    ''' <summary>
+    ''' Interaction logic for SampleWindow.xaml
+    ''' </summary>
+    Partial Public Class SampleWindow
+        Inherits Window
+
+        'public static readonly DependencyProperty TabCaptionProperty;
+
+        'static SampleWindow() {
+        '    TabCaptionProperty = DependencyProperty.RegisterAttached("TabCaption", typeof(string), typeof(SampleWindow));
+        '}
+
+        'public static string GetTabCaption(DependencyObject obj) {
+        '    return (string)obj.GetValue(TabCaptionProperty);
+        '}
+        'public static void SetTabCaption(DependencyObject obj, string value) {
+        '    obj.SetValue(TabCaptionProperty, value);
+        '}
+
+        Public Sub New()
+            InitializeComponent()
+
+            'pnlProduct.CaptionTemplate = FindResource("AzureCaptionTemplate") as DataTemplate;
+            'pnlEmployee.CaptionTemplate = FindResource("AzureCaptionTemplate") as DataTemplate;
+
+            'SetTabCaption(pnlProduct, "Tab 1");
+            'SetTabCaption(pnlEmployee, "Tab 2");
+        End Sub
+    End Class
+End Namespace
